6535ad9ca81e81ada998163130eea59ee33a867c
Mauro <ma.candias@duocuc.cl>
2022-05-04 17:44:15 -0400

    modulo recepcion agregado

--- intamed_app/intamed_app/settings.py
+++ intamed_app/intamed_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modulo_medico',
+    'modulo_recepcion'
 ]
 
 MIDDLEWARE = [
--- intamed_app/intamed_app/urls.py
+++ intamed_app/intamed_app/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('medico/',include('modulo_medico.urls'))
+    path('medico/',include('modulo_medico.urls')),
+    path('recepcion/',include('modulo_recepcion.urls'))
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
