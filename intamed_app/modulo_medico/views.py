f2e3b9600a6ffa2227819dadd98dbe91a503ff9f
Mauro <ma.candias@duocuc.cl>
2022-06-03 21:09:29 -0400

    asda

--- intamed_app/modulo_medico/views.py
+++ intamed_app/modulo_medico/views.py
@@ -1,8 +1,23 @@
+from multiprocessing import context
+from multiprocessing.sharedctypes import Array
+from select import select
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Comuna, EstadoCivil, Genero, Medico, Nacionalidad, Paciente, Usuario, Atencion, Farmaco, Prevision, TelefonoUsuario, Telefono
+from .models import TipoFarmaco, PerfilUsuario, Administrador, Recepcionista, Contrato, TipoContrato, Alergia, DetalleAlergia
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.decorators import login_required,permission_required
+from django.views.decorators.csrf import csrf_protect
+from datetime import datetime
+from django.db.models import Count
+from django.contrib import messages
+import logging
+from django.http import JsonResponse
+import datetime
 # Create your views here.
 
 def inicio(request):
+    user= Medico.Object.all()
     return render(request,"base_medico.html")
 
   
