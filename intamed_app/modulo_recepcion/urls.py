cdd6f0caaec6f24f96f09ff96fd06b67c31e6f90
maalbornozc <87202075+maalbornozc@users.noreply.github.com>
2022-06-20 19:39:20 -0400

    Edit's

--- intamed_app/modulo_recepcion/urls.py
+++ intamed_app/modulo_recepcion/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import inicio, ingresarPago, filtro_pacientes, editar_paciente
+from .views import inicio, ingresarPago, filtro_pacientes, editar_paciente, anularHora
 
 urlpatterns = [   
     path('inicio/ingresarPac',inicio, name="ingresarPac"),
     path('inicio/buscarPac',filtro_pacientes, name="buscarPac"),
     path('inicio/ingresarPago',ingresarPago, name="ingresarPag"),
-    path('inicio/editararPac/<id>',editar_paciente, name="editarPac"),
+    path('inicio/editarPac/<id>',editar_paciente, name="editarPac"),
+    path('inicio/anularHora',anularHora, name="anularHor")
 ]
