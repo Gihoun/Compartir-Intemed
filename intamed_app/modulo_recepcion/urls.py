922d8c5dacd7fcc8554abb7080407825ff28ee27
andreinacat <andrea.verdugomunoz@gmail.com>
2022-07-13 03:12:13 -0400

    tomar hora recepcion

--- intamed_app/modulo_recepcion/urls.py
+++ intamed_app/modulo_recepcion/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import inicio, ingresarPago, filtro_pacientes, editar_paciente, anularHora, genpdf_boleta
+from .views import inicio, ingresarPago, filtro_pacientes, editar_paciente, anularHora, genpdf_boleta, tomar_hora
 
 app_name="modulo_recepcion"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('inicio/ingresarPago',ingresarPago, name="ingresarPag"),
     path('inicio/editarPac/<id>',editar_paciente, name="editarPac"),
     path('inicio/anularHora',anularHora, name="anularHor"),
-    path('inicio/genboleta/<id>',genpdf_boleta, name="genpdf")
+    path('inicio/genboleta/<id>',genpdf_boleta, name="genpdf"),
+    path('inicio/tomarHora/',tomar_hora, name="tomar_hr")
 ]
